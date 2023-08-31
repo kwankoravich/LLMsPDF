c34ed71eb9b2bd94ca0a3cd7ffc3d6aecf115677
Koravich Sangkaew <41057176+kwankoravich@users.noreply.github.com>
2023-08-31 22:07:13 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks,openai_key):
-    embeddings = OpenAIEmbeddings(openai_key)
+    embeddings = OpenAIEmbeddings(openai_api_key = openai_key)
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
